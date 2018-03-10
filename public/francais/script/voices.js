66f50463eafec759f5c1b77b65eb8b11222383a2
Stoyan <ssttoo@ymail.com>
2018-03-10 15:25:23 -0800

    Initial commit

---
+++ public/francais/script/voices.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+const spawn = require('child_process').spawnSync;
+
+const readDir = fs.readdirSync;
+
+const dataDir = path.resolve(__dirname, '../json');
+
+function justLetters(s) {
+  return s
+    .toLowerCase()
+    .replace(/à/g, 'a')
+    .replace(/é/g, 'e')
+    .replace(/\W/g, '');
+}
+
+readDir(dataDir).forEach(f => {
+  if (f.startsWith('.')) {
+    return; // no .DS_Store etc, thank you
+  }
+  const file = path.resolve(dataDir, f);
+  const jsonData = require(file);
+  [
+    'Amelie',
+    'Audrey',
+    'Aurelie',
+    'Chantal',
+    'Nicolas',
+    'Thomas',
+  ].forEach(voice => {    
+    jsonData.forEach(definition => {
+      const word = definition[0];
+      const outfile = `voices/${voice}/${justLetters(word)}`; // .aiff is assumed
+      console.log(outfile);
+      spawn('say', ['-v', voice, '-o', outfile, word]);
+    });
+  });
+});
+
+// for f in *.aiff; do ffmpeg -i $f "${f%.*}.mp3"; done
+// rm -rf *.aiff
